f8e8d6629d11976e7b8ca3a647babea6d7b536ba
Carlos Henrique <carloshenriquesantanas19@gmail.com>
2022-05-22 23:05:59 -0300

    ♻️ refactor user entity

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -2,26 +2,26 @@ import * as TypeORM from "typeorm"
   
 @TypeORM.Entity("users")
 export class User {
-    @TypeORM.PrimaryGeneratedColumn("uuid")
-    readonly id: string
+  @TypeORM.PrimaryGeneratedColumn("uuid")
+  readonly id: string
 
-    @TypeORM.Column()
-    name: string
+  @TypeORM.Column()
+  name: string
 
-    @TypeORM.Column({unique: true})
-    email: string
+  @TypeORM.Column({unique: true})
+  email: string
 
-    @TypeORM.Column({select: false})
-    password: string
+  @TypeORM.Column({select: false})
+  password: string
 
-    @TypeORM.Column()
-    age: number
+  @TypeORM.Column()
+  age: number
 
-    @TypeORM.CreateDateColumn({
-      name: "created_at",
-    })
-    createdAt: Date
+  @TypeORM.CreateDateColumn({
+    name: "created_at",
+  })
+  createdAt: Date
 
-    @TypeORM.UpdateDateColumn({ name: "updated_at" })
-    updatedAt: Date
+  @TypeORM.UpdateDateColumn({ name: "updated_at" })
+  updatedAt: Date
 }
